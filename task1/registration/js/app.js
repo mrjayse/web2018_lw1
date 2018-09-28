3dad5fd64165a20a1db5a633058eb902c6ff615e
ioouKZjxBN5psUpnh3s4VdUq <mc.mjayse@gmail.com>
2018-09-28 22:42:11 +0400

    accept code of conduct

--- task1/registration/js/app.js
+++ task1/registration/js/app.js
@@ -1,6 +1,6 @@
-function checkEmail(str){
+function checkEmail(str) {
   var result = str.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
-  if (!result){
+  if (!result) {
     alert('Введите валидный e-mail.');
     return false;
   }
