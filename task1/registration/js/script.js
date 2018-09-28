2a473df662c4762ca05de36f55f1a29880c051bc
ioouKZjxBN5psUpnh3s4VdUq <mc.mjayse@gmail.com>
2018-09-28 22:11:01 +0400

    initial commit

---
+++ task1/registration/js/script.js
@@ -0,0 +1,43 @@
+function checkEmail(str){
+  var result = str.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
+  if (!result){
+    alert('Введите валидный e-mail.');
+    return false;
+  }return true;
+}
+
+function checkPassword(pass, confrim) {
+  if (pass.length < 6) {
+    alert('Длина пароля должна быть не менее 6-ти символов.');
+    return false;
+  }
+  if (pass = confrim) {
+    alert('Введенные пароли не совпадают.');
+    return false;
+  }
+  return true;
+}
+
+function checkAgreementRules() {
+  var result = document.getElementsByClassName("registration__check-confirmation")[0].checked;
+  if (!result) {
+    alert("Примите условия соглашения!");
+    return false;
+  }
+  return true;
+}
+
+function registration();
+{
+  var email = document.getElementsByClassName("registration__login")[0].value;
+  var pass = document.getElementsByClassName("password-block__password")[0].value;
+  varpassConfirm = document.getElementsByClassName("password-block__password-repeat")[0].value;
+  if (checkEmail(email) && checkPassword(pass, passConfirm)
+      && checkAgreementRules()) {
+    alert("Регистрация прошла успешно!");
+  }
+}
+
+document.getElementsByClassName("registration")[0].onsubmit = function() {
+  Registration();
+};
---
+++ task4/group-subscribe.js
@@ -0,0 +1,23 @@
+const myGroups = []; // массив для хранения подписок на группы
+
+const groupList = [
+  { groupName: 'Habrahabr'},
+  { groupName: 'Web2018'},
+];
+
+/**
+ * Функция подписки на группы
+ * @param group
+ */
+function subscribeGroup(group) {
+  // todo
+  myGroups.push(group);
+}
+
+/**
+ * Функция отписки от группы
+ * @param group
+ */
+function unsubscribeGroup(group) {
+  // todo
+}
